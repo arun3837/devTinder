a8d9d71d8974f990893c13e92d4eece51baad9c7
Shwetha <shwetha11405@gmail.com>
2025-03-25 11:46:36 +0530

    multiple-router-handler

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const app = express();
 
+app.use( "/user", 
+    (req,res, next) => {
+        console.log("Hello one");
+        //res.send("Response 1");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Hello two");
+        //res.send("Response two");
+        next();
+    },
+    (req, res) => {
+        console.log("Hello three");
+        res.send("Hello three");
+    } 
+);
+
 app.listen(3000, () => {
     console.log("Listening from the server");
+} );
+
+/* app.post("/addUser", (req, res) => {
+    console.log ( req.body  );
+    res.send("user data added successfully");
+} );
+
+app.get("/getUser", (req, res) => {
+    res.send({"firstname" : "Arun", "last_name" : "kumar" });
+} );
+
+app.post("/updateUser", (req, res) => {
+    res.send("user data updated successfully");
+} );
+
+app.delete("/deleteUser", (req, res) => {
+    res.send("user data deleted successfully");
 } );
 
 app.use("/dev", (req, res) => {
@@ -19,6 +53,6 @@ app.use("/prod", (req, res) => {
 
 app.use("/", (req, res) => {
     res.send("Namaste JS");
-} );
+} ); */
 
 //console.log("Hello world");
